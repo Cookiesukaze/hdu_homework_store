afa7f167f957f65870dd56778982a73c65083a4a
Cookiesukaze <76832331+Cookiesukaze@users.noreply.github.com>
2023-09-28 18:16:34 +0800

    Add files via upload

---
+++ 自然语言处理/2提取高频词/extract_high_freq_words.py
@@ -0,0 +1,34 @@
+import jieba
+
+
+# 分词
+def divide_text(text_path, stop_word_path, only_vital_chinese):
+    text = open(text_path, encoding='UTF-8').read()
+    stop_word = open(stop_word_path, encoding='UTF-8').read().splitlines()
+    jieba.load_userdict(stop_word)  # 添加分词词典txt
+    words = jieba.lcut(text)  # 分词并返回列表
+    if only_vital_chinese:
+        words = [word for word in words if word not in stop_word]  # 去掉stop_word里面的词
+        words = [word for word in words if word != '\n']  # 去掉换行符
+    # print(words)
+    return words
+
+
+# 排序
+def statistics_func(text_list):
+    dictionary = {}  # 创建字典
+    for i in text_list:
+        if i not in dictionary:
+            dictionary[i] = 1
+        else:
+            dictionary[i] += 1
+    # print(dictionary)
+    sorted_dic = dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))  # 按值倒序排序并重新生成字典
+    # print(sorted_dic)
+    for k, v in list(sorted_dic.items())[:10]:  # 转列表输出前十项
+        print(k, v)
+
+
+if __name__ == "__main__":
+    divided_text = divide_text(text_path="flightnews.txt", stop_word_path="stopword.txt", only_vital_chinese=True)
+    statistics_func(divided_text)
